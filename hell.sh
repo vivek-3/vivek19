ef7626cece7942bd40219dff44ed0e349c1849a6
pawankopparthi <pawankopparthi@sidgs.com>
2022-02-14 19:44:02 +0530

    first commit

--- hell.sh
+++ hell.sh
@@ -1,4 +1,4 @@
-curl -L -u pawankopparthi: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 https://github.com/orgs/pawansidgs/repositories -d '{"name":"test13","private":true}'
+curl -L -u pawankopparthi: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 https://github.com/orgs/pawansidgs/repos -d '{"name":"test13","private":true}'
 touch hand.txt
 git init
 git add .
@@ -7,3 +7,4 @@ git config user.name pawankopparthi
 git commit -m "first commit"
 git branch -M main
 git push origin -u git push https://pawankopparthi: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@github.com/sidgs/test13.git  main
+
